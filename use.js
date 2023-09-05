d742c92e95b43950acac19e1d3a48fd82746c9c7
aeonmerx <103615875+aeonmerx@users.noreply.github.com>
2023-09-05 01:02:48 -0500

    Update use.js

--- use.js
+++ use.js
@@ -1,22 +1,22 @@
-// Usar las funciones y datos del encriptador directamente desde el módulo encriptador.js
+// Importar el módulo encriptador (no necesitas 'require' en un navegador web)
+// encriptador.js ya debe estar incluido en tu página HTML antes de este script
+// Puedes acceder a las funciones y datos directamente
+
+// Usar las funciones y datos del encriptador
 const modificador = 24;
 const mensaje = 'AEON MERX';
 
-// Encriptar el mensaje
-const mensajeEncriptado = encriptador.encriptar(modificador, mensaje);
+const mensajeEncriptado = encriptar(modificador, mensaje);
 console.log('Mensaje encriptado:', mensajeEncriptado);
 
-// Desencriptar el mensaje
-const mensajeDesencriptado = encriptador.desencriptar(modificador, mensajeEncriptado);
+const mensajeDesencriptado = desencriptar(modificador, mensajeEncriptado);
 console.log('Mensaje desencriptado:', mensajeDesencriptado);
 
-// Calcular la entropía del mensaje encriptado
-const entropia = encriptador.calcularEntropia(mensajeEncriptado);
+const entropia = calcularEntropia(mensajeEncriptado);
 console.log('Entropía del mensaje:', entropia);
 
-// Acceder a los datos de colores y planetas directamente desde el módulo encriptador.js
-console.log('Colores:', encriptador.colores);
-console.log('Planetas:', encriptador.planetas);
+console.log('Colores:', colores);
+console.log('Planetas:', planetas);
 
 // Convertir números en el mensaje a colores
 const mensajeConColores = encriptador.convertirNumerosAColores(mensajeEncriptado);
