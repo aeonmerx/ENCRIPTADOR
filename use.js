b5c1ba69db065971763ea736e7bd525f8805e69e
aeonmerx <103615875+aeonmerx@users.noreply.github.com>
2023-09-05 00:53:26 -0500

    Update use.js

--- use.js
+++ use.js
@@ -1,40 +1,27 @@
-// Espera a que el encriptador.js se cargue completamente
-document.querySelector('script[src="https://aeonmerx.github.io/ENCRIPTADOR/encriptador.js"]').addEventListener('load', function () {
-  // El encriptador.js se ha cargado, ahora puedes usar sus funciones y datos
+// Usar las funciones y datos del encriptador directamente desde el módulo encriptador.js
+const modificador = 24;
+const mensaje = 'AEON MERX';
 
-  // Pedir al usuario que ingrese el modificador
-  const modificadorInput = prompt('Ingrese el modificador (número entero):');
-  const modificador = parseInt(modificadorInput);
+// Encriptar el mensaje
+const mensajeEncriptado = encriptador.encriptar(modificador, mensaje);
+console.log('Mensaje encriptado:', mensajeEncriptado);
 
-  // Pedir al usuario que ingrese el mensaje
-  const mensaje = prompt('Ingrese el mensaje (texto):');
+// Desencriptar el mensaje
+const mensajeDesencriptado = encriptador.desencriptar(modificador, mensajeEncriptado);
+console.log('Mensaje desencriptado:', mensajeDesencriptado);
 
-  // Verificar si el modificador es un número entero válido
-  if (!isNaN(modificador) && Number.isInteger(modificador)) {
-    // Encriptar el mensaje
-    const mensajeEncriptado = encriptar(modificador, mensaje);
-    console.log('Mensaje encriptado:', mensajeEncriptado);
+// Calcular la entropía del mensaje encriptado
+const entropia = encriptador.calcularEntropia(mensajeEncriptado);
+console.log('Entropía del mensaje:', entropia);
 
-    // Desencriptar el mensaje
-    const mensajeDesencriptado = desencriptar(modificador, mensajeEncriptado);
-    console.log('Mensaje desencriptado:', mensajeDesencriptado);
+// Acceder a los datos de colores y planetas directamente desde el módulo encriptador.js
+console.log('Colores:', encriptador.colores);
+console.log('Planetas:', encriptador.planetas);
 
-    // Calcular la entropía del mensaje encriptado
-    const entropia = calcularEntropia(mensajeEncriptado);
-    console.log('Entropía del mensaje:', entropia);
+// Convertir números en el mensaje a colores
+const mensajeConColores = encriptador.convertirNumerosAColores(mensajeEncriptado);
+console.log('Mensaje con colores:', mensajeConColores);
 
-    // Acceder a los datos de colores y planetas directamente desde el módulo encriptador.js
-    console.log('Colores:', colores);
-    console.log('Planetas:', planetas);
-
-    // Convertir números en el mensaje a colores
-    const mensajeConColores = convertirNumerosAColores(mensajeEncriptado);
-    console.log('Mensaje con colores:', mensajeConColores);
-
-    // Convertir colores en el mensaje a planetas
-    const mensajeConPlanetas = convertirColoresAPlanetas(mensajeConColores);
-    console.log('Mensaje con planetas:', mensajeConPlanetas);
-  } else {
-    console.log('El modificador ingresado no es un número entero válido.');
-  }
-});
+// Convertir colores en el mensaje a planetas
+const mensajeConPlanetas = encriptador.convertirColoresAPlanetas(mensajeConColores);
+console.log('Mensaje con planetas:', mensajeConPlanetas);
